bb88bbf2663d4f24404a918a0ef35171fcf9a02f
Anubhav Agarwal <anubhav.ag@gmail.com>
2020-11-29 23:37:55 +0800

    backend for login and register

---
+++ index.js
@@ -0,0 +1,76 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
+const usersController = require('./controllers/UserController')
+
+const app = express();
+const port = process.env.PORT;
+
+const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`
+mongoose.set('useFindAndModify', false)
+
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(cors({
+  origin: '*'
+}))
+
+app.options('*', cors())
+
+/*
+USER ON-BOARDING ROUTES
+*/
+
+//user registration
+app.post('/api/v1/users/register', usersController.register)
+
+// user login route
+app.post('/api/v1/users/login', usersController.login)
+
+
+// connect to DB, then inititate Express app
+mongoose.connect( mongoURI, { useNewUrlParser: true, useUnifiedTopology: true } )
+  .then(response => {
+    // DB connected successfully
+    console.log('DB connection successful')
+
+    app.listen(process.env.PORT || port, () => {
+      console.log(`Delivery Pool app listening on port: ${port}`)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+function verifyJWT(req, res, next) {
+  // get the jwt token from the request header
+  const authToken = req.headers.auth_token
+  
+  // check if authToken header value is empty, return err if empty
+  if (!authToken) {
+    res.json({
+      success: false,
+      message: "Auth header value is missing"
+    })
+    return
+  }
+
+  // verify that JWT is valid and not expired
+  try {
+    // if verify success, proceed
+    const userData = jwt.verify(authToken, process.env.JWT_SECRET, {
+      algorithms: ['HS384']
+    })
+    next()
+  } catch(err) {
+    // if fail, return error msg
+    res.json({
+      success: false,
+      message: "Auth token is invalid"
+    })
+    return
+  }
+}
---
+++ models/users.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        //required: true,
+        max: 100
+    },
+    last_name: {
+        type: String,
+        //required: true,
+        max: 100
+    },
+    contact_number:{
+        type: Number
+    },
+    user_id: {
+        type: String,
+        unique: true
+    },
+    slug: {
+        type: String,
+        unique:true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        max: 100
+    },
+    default_address: {
+        type: String,
+        //required: true,
+        default: Date.now
+    },
+    pwsalt: {
+        type: String,
+        required: true
+    },
+    hash: {
+        type: String,
+        required: true
+    }
+}
+)
+
+const UserModel = mongoose.model('User', userSchema)
+
+module.exports = UserModel
